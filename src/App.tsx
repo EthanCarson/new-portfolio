831fd5706cd3270effbeeb75e67c9e931dbc56af
Ethan Carson <190144898+EthanCarson@users.noreply.github.com>
2025-07-03 14:08:20 -0400

    feat: enhance App component layout and styling, update CSS for font integration

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,16 @@ import Project from "./components/Project";
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <h1 class="text-green-300 font-press-start">BIG TITLE HERE</h1>
-      <img src="" alt="Logo" />
-      <h2>
+      <h1 class="mb-4 text-4xl font-extrabold leading-none tracking-tight text-green-500 md:text-5xl lg:text-6xl mt-30">BIG TITLE HERE</h1>
+      <img src="" alt="Logo" class="inline"/>
+      <h2 class="inline ml-5 text-2xl">
         <a href="">Download Resume</a>
       </h2>
 
-      <article>
-        <section>
-          <img src="" alt="Profile Picture" />
-          <p>
+      <article class="mt-8">
+        <section class="flex flex-row items-center justify-between">
+          <img src="" alt="Profile Picture" class="inline flex-1/4"/>
+          <p class="inline flex-3/4 text-sm text-left indent-12">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
             mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien
             fringilla, mattis ligula consectetur, ultrices mauris. Maecenas
